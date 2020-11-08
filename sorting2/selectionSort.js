9fb918d8050812d3388bd6d3b77d0ae944d6357c
kolinkuang <kolinkuang@github.com>
2020-11-08 21:45:29 +0800

    selectionSort

---
+++ sorting2/selectionSort.js
@@ -0,0 +1,24 @@
+function selectionSort(list) {
+    const n = list.length;
+
+    if (!n) {
+        return [];
+    }
+
+    let minIndex;
+    for (let i = 0; i < n - 1; i++) {
+        minIndex = i;
+        for (let j = i + 1; j < n; j++) {
+            if (list[j] < list[minIndex]) {
+                minIndex = j;
+            }
+        }
+        [list[i], list[minIndex]] = [list[minIndex], list[i]];
+    }
+
+    return list;
+}
+
+console.log(selectionSort([4, 5, 6, 1, 3, 2]));
+console.log(selectionSort([6, 5, 4, 3, 2, 1]));
+console.log(selectionSort([3, 5, 4, 1, 2, 6]));
